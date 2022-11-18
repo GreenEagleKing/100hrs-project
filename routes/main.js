f25f38e6e2717322fc8dd36c120b180315464dcf
Ben King <benking1293@gmail.com>
2022-11-18 11:24:11 +0000

    "Created login & signup feature"

--- routes/main.js
+++ routes/main.js
@@ -6,5 +6,11 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.post("/signup", authController.postSignup);
+router.get("/signup", authController.getSignup)
+router.post("/login", authController.postLogin)
+router.get("/login", authController.getLogin);
+router.get("/logout", authController.logout)
+
 
 module.exports = router;
