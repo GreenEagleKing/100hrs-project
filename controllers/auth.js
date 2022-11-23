21a26e7fa7ff1c59647624854546c9654c8b1582
Ben King <benking1293@gmail.com>
2022-11-23 15:49:24 +0000

    "Fixed login and added some styling"

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,6 +12,7 @@ exports.getLogin = (req, res) => {
 };
 
 exports.postLogin = (req, res, next) => {
+  console.log(req.body.email)
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
@@ -44,15 +45,17 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    console.log("User has logged out.");
+    req.session.destroy((err) => {
+      if (err) console.log("Error : Failed to destroy the session during logout.", err);
+      req.user = null;
+      res.redirect("/");
+    });
   });
 };
 
--- routes/main.js
+++ routes/main.js
@@ -7,13 +7,13 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-router.get("/library", ensureAuth, productController.getLibrary);
+router.get("/library", productController.getLibrary);
 router.get("/profile", ensureAuth, productController.getProfile);
 router.post("/signup", authController.postSignup);
-router.get("/signup", authController.getSignup)
-router.post("/login", authController.postLogin)
+router.get("/signup", authController.getSignup);
+router.post("/login", authController.postLogin);
 router.get("/login", authController.getLogin);
-router.get("/logout", authController.logout)
+router.get("/logout", authController.logout);
 
 
 module.exports = router;
