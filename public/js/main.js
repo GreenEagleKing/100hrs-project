3fade206f98309c8e820422634270205fe06dfb4
Ben King <benking1293@gmail.com>
2022-12-16 12:54:34 +0000

    "Add a product styling in dashboard"

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,27 @@ function toggleNavBlue() {
     let url = document.documentURI.match(/.*\/(.*)\/(.*)$/)[2]
     if(url === "") {
         url = 'home'
+        document.querySelector(`#${url}`).classList.add('md:text-blue-700')
     }
-    document.querySelector(`#${url}`).classList.add('md:text-blue-700')
     
+
 }
 toggleNavBlue()
+
+// Dropdown to add product form appears on click
+
+const addProductBtn = document.querySelector('#addProductBtn').addEventListener('click', showAddProduct)
+
+function showAddProduct() {
+    document.getElementById('addProduct').classList.toggle('hidden')
+}
+
+
+function showImgPreview(event){
+    if(event.target.files.length > 0){
+      const src = URL.createObjectURL(event.target.files[0]);
+      let preview = document.getElementById("file-ip-1-preview");
+      preview.src = src;
+      preview.style.display = "flex";
+    }
+  }
